73bee415bedcd450af53446d560495ff1264836c
Anna <tuola@list.ru>
2023-10-19 16:22:48 +0300

    Решена вторая задача

--- js/arrays.js
+++ js/arrays.js
@@ -4,5 +4,12 @@ function task1()
     console.log(arr.toString());
     let sum=arr.filter(index=>index%2==0).reduce((sum,elem)=>sum+Math.sqrt(elem),0);
     console.log(sum);
+}
 
+function task2()
+{
+    let arr=Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
+    console.log(arr.toString());
+    let sum=arr.filter(elem=>elem>0 && elem<10).reduce((sum,elem)=>sum+elem,0);
+    console.log(sum);
 }
